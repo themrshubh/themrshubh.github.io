165288cd0eadf69f0341d848c027b5b08e860131
Shubham Mankar <themrshubh@gmail.com>
2021-03-07 18:41:07 +0530

    init dev

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+  vueIndentScriptAndStyle: true,
+}
