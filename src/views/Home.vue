58197001916e53ebd23813525bd27721c0117b2a
Shubham Mankar <themrshubh@gmail.com>
2021-03-13 19:15:30 +0530

    added home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="home">
-    <h1 class="text-primary">Hello World</h1>
+  <div class="container-fluid p-0 bg-light">
+    <section id="hero" class="p-4 p-md-5 border-bottom border-5">
+      <hero />
+    </section>
+    <section id="about" class="bg-danger p-4 p-md-5 border-bottom border-5">
+      <about />
+    </section>
+    <section id="work" class="p-4 p-md-5 border-bottom border-5">
+      <work />
+    </section>
   </div>
 </template>
 
 <script>
+  import Hero from '../components/Hero'
+  import About from '../components/About'
+  import Work from '../components/Work'
+
   export default {
     name: 'Home',
+
+    components: {
+      Hero,
+      About,
+      Work,
+    },
   }
 </script>
