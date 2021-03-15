63b4b3d222657e4cf2f28bee61e0cb880c250603
Shubham Mankar <themrshubh@gmail.com>
2021-03-15 19:41:02 +0530

    fixed projects v-for

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,21 +2,16 @@
   <div class="w-100">
     <h2 class="py-2 text-center text-dark">Projects</h2>
     <div class="container-fluid my-5">
-      <div v-for="i in numOfRows" :key="i" class="row">
-        <div class="col-md-6 mb-4">
-          <project
-            :title="projects[i - 1].title"
-            :lang="projects[i - 1].lang"
-            :year="projects[i - 1].year"
-          />
-        </div>
-        <div v-if="i < projects.length" class="col-md-6 mb-4">
-          <project
-            :title="projects[i].title"
-            :lang="projects[i].lang"
-            :year="projects[i].year"
-          />
-        </div>
+      <div v-for="index in numOfRows" :key="index" class="row">
+        <template v-for="i in getIndices(index)" :key="`${index}${i}`">
+          <div v-if="i < projects.length" class="col-md-6 mb-4">
+            <project
+              :title="projects[i].title"
+              :lang="projects[i].lang"
+              :year="projects[i].year"
+            />
+          </div>
+        </template>
       </div>
     </div>
   </div>
@@ -44,6 +39,17 @@
           { title: 'Sentiment Analyzer', lang: 'Python', year: '2019' },
         ],
       }
+    },
+
+    methods: {
+      getIndices(i) {
+        // Returns the index in projects for column
+        // Note that the index (i in numOfRows) starts from 1
+        // Return value examples
+        // Ex. i = 1 => return 0,1
+        //Ex. i = 2 => return 2,3
+        return [2 * i - 2, 2 * i - 1]
+      },
     },
   }
 </script>
