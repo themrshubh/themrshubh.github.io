f61e359489b0be0484289e44416371671d097295
Shubham Mankar <themrshubh@gmail.com>
2021-03-16 16:06:38 +0530

    added interests

---
+++ src/components/Interests.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 class="py-2 text-center text-light">
+      What's up
+    </h2>
+    <p class="p-3 p-md-5 fs-4 fw-light text-light">
+      Apart from being a full time student, I am exploring iOS Development using
+      Swift owing to my interest in software development and the Apple. I am
+      also getting started in Open Source software development. In my free time,
+      I love watching TV Shows &nbsp;&mdash;&nbsp; some of my favorite ones are
+      Modern Family and the Office. Other than that, I like learning about
+      productivity and
+      <a
+        href="https://www.theminimalists.com/"
+        class="text-primary fw-normal text-decoration-none"
+        target="_blank"
+      >
+        Minimalism.
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        hover: false,
+      }
+    },
+  }
+</script>
+
+<style scoped></style>
