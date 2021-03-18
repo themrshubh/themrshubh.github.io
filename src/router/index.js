459e3bcd7026445d613940b91bcbf3976287e10c
Shubham Mankar <themrshubh@gmail.com>
2021-03-18 12:03:32 +0530

    added lazy routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import NotFound from '../views/NotFound.vue'
+
+const NotFound = () => import('../views/NotFound.vue')
 
 const routes = [
   {
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="container-fluid p-0">
+  <div class="container-fluid p-0 bg-light" style="min-height: 100vh">
     <section id="error" class="p-4 p-md-5 text-center">
       <h1 class="display-1 text-danger">
         404
       </h1>
-      <h1 class="fw-light text-dark mb-5">
+      <h1 class="fw-light text-dark mb-2">
         Not Found
       </h1>
-      <router-link class="btn btn-secondary text-white fs-6" to="/">
-        Take me back
+      <h4 class="fw-light text-dark mb-5">
+        Hey! Looks like you've got a wrong link.
+      </h4>
+      <router-link class="btn btn-dark text-white fs-6" to="/">
+        Home
       </router-link>
     </section>
   </div>
