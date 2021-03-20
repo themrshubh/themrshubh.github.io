453748d8fa65acc51be4e0845bf863950b804ad7
Shubham Mankar <themrshubh@gmail.com>
2021-03-20 18:27:30 +0530

    added support for vue-router  history mode in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,16 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# copy index.html -> 404.html for Vue to work in history mode with Github Pages
+cp index.html 404.html
 
+echo copied!
+
+# if you are deploying to a custom domain
+echo 'www.shubhammankar.com' > CNAME
+
+# deploy: push to github
+echo Deploying
 git init
 git add -A
 git commit -m 'Deploy'
